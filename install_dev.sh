ec3c36ffc3a51a20162bb008d0ba12e50dffc087
Anke Heijnen <aheijnen@pronexus.nl>
2014-10-16 23:56:06 +0200

    Vendor en CDN upgrade. Install voortaan voor global composer

--- install_dev.sh
+++ install_dev.sh
@@ -9,27 +9,27 @@ echo "-------------------------------------"
 echo ""
 echo "- Installeren van vendor pakketten"
 echo ""
-php composer.phar install --optimize-autoloader --quiet
+composer install --optimize-autoloader --quiet
 
 echo ""
 echo "- Cache en logs initialiseren"
 echo ""
-php app/console cache:clear
+app/console cache:clear
 chmod 773 app/cache
 chmod 773 app/logs
 
 echo ""
 echo "- Installeren van assets in web directory"
 echo ""
-php app/console assets:install
-php app/console assetic:dump
+app/console assets:install
+app/console assetic:dump
 
 echo ""
 echo "- Aanmaken en vullen van databasetabellen"
 echo ""
-php app/console doctrine:schema:drop --force
-php app/console doctrine:schema:create
-php app/console widop:fixtures:load --env=dev
+app/console doctrine:schema:drop --force
+app/console doctrine:schema:create
+app/console widop:fixtures:load --env=dev
 
 echo ""
 echo "-------------------------------------"
--- install_prod.sh
+++ install_prod.sh
@@ -9,26 +9,26 @@ echo "-------------------------------------"
 echo ""
 echo "- Installeren van vendor pakketten"
 echo ""
-php composer.phar install --no-dev --optimize-autoloader --quiet
+composer install --no-dev --optimize-autoloader --quiet
 
 echo ""
 echo "- Cache en logs initialiseren"
 echo ""
-php app/console cache:clear --env=prod --no-debug
+app/console cache:clear --env=prod --no-debug
 chmod 773 app/cache
 chmod 773 app/logs
 
 echo ""
 echo "- Installeren van assets in web directory"
 echo ""
-php app/console assets:install
-php app/console assetic:dump --env=prod --no-debug
+app/console assets:install
+app/console assetic:dump --env=prod --no-debug
 
 echo ""
 echo "- Bijwerken en vullen van databasetabellen"
 echo ""
-php app/console doctrine:schema:update --force
-php app/console widop:fixtures:load --env=prod
+app/console doctrine:schema:update --force
+app/console widop:fixtures:load --env=prod
 
 echo ""
 echo "-------------------------------------"
